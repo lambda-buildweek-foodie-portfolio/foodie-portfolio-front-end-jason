b1a8dc59177646750b6529c823a1c549b2c624d1
unknown <jjamal95@gmail.com>
2019-05-09 22:34:22 -0500

    Main structure of app is setup, started on search bar and header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
 import './App.css';
+import SearchBar from './Components/SearchBar/SearchBar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import Header from './Components/Header/Header';
+
+library.add(faStroopwafel)
 
 function App() {
   return (
     <div className="App">
-    
+      <Header/>
+      <SearchBar/>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { 
+  HeaderH1, 
+  HeaderDiv, 
+  HeaderSpans,
+} from '../Styling/styles'
+
+
+
+import { connect } from 'react-redux';
+import Moment from '../Moment/moment';
+
+class Header extends Component{
+  state = {
+    user: {
+        username: '',
+        password: '',
+    }
+}
+
+homePageClick = () => {
+  this.props.history.push('/');
+}
+  
+  render() {
+    return (
+      <div>
+        <HeaderDiv>
+          <HeaderSpans date> <Moment/></HeaderSpans>
+          <HeaderH1 >Foodie Portfollio</HeaderH1> 
+         
+          <HeaderSpans temp>80°</HeaderSpans>   
+        </HeaderDiv>
+      </div>
+    )
+  }
+}
+// export default Header
+export default Header; 
